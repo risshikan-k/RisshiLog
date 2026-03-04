aafe4eca74581259d1d6da188006e73add824878
Daisuke <okka0727@gmail.com>
2026-03-04 20:32:36 +0900

    url

--- web/config.js
+++ web/config.js
@@ -1,4 +1,4 @@
 // 塾のアカウントで新しくデプロイしたGASのURLを入れる場所
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyChITOAPOUtPHtet9U08r2j-vLWolN69ubLFVShDGlFDzapk9WHopOV6F-4fghTv5P/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbyChITOAP0UtPHtet9U08r2j-vLWolN69ubLFVShDGlFDzapk9WHopOV6F-4fghTv5P/exec"
 };
